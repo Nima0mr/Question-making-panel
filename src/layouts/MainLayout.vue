84b5a7423d74148e98d9921bcc2f2a2daff0ca78
Nima.mr <nima1380620@yahoo.com>
2022-08-21 16:31:45 +0430

    MainLayout.vue types updated!

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,17 +1,17 @@
-<!--       -----------------------ToDO => change dashboard route name ------------------------ -->
+<!--       -----------------------ToDO => change last v-else-ifs ------------------------ -->
 
 <template>
   <quasar-template-builder v-model:value="properties" @onResize="resize">
     <template #header>
-      <div v-if="templateHeaderType === 'onlineQuiz.alaaView'" class="header-inside row">
+      <div v-if="templateHeaderType === 'quiz'" class="header-inside row">
         <online-quiz-template-header/>
       </div>
-      <div v-else-if="templateHeaderType === 'A'" class="user-main-layout-header">
+      <div v-else-if="templateHeaderType === 'default'" class="user-main-layout-header">
         <div class="header-inside row">
           <user-template-header/>
         </div>
       </div>
-      <div v-else class="main-layout-header row">
+      <div v-else-if="templateHeaderType === 'panel'" class="main-layout-header row">
           <template-header/>
       </div>
       <q-linear-progress
@@ -24,10 +24,10 @@
       <q-resize-observer @resize="setHeaderDimension"/>
     </template>
     <template #left-drawer>
-      <div class="drawer-inside-of-MapOfQuestions" v-if="$route.name === 'onlineQuiz.alaaView'">
+      <div class="drawer-inside-of-MapOfQuestions" v-if="templateSideBarType === 'quiz'">
         <sideMenuMapOfQuestions/>
       </div>
-      <div class="drawer-inside" v-else>
+      <div class="drawer-inside" v-else-if="templateSideBarType === 'panel'">
         <side-menu-dashboard/>
       </div>
     </template>
@@ -69,6 +69,7 @@ export default {
     return {
       keepAliveComponents: KeepAliveComponents,
       templateHeaderType: '',
+      templateSideBarType: '',
       properties: {
         layoutView: 'lHh Lpr lFf',
         layoutHeader: true,
@@ -96,30 +97,17 @@ export default {
     confirmDialogData () {
       return this.$store.getters['AppLayout/confirmDialog']
     },
-    getTemplateHeaderType () {
+    getTemplateTypes () {
       return () => {
-        // this.$store.dispatch('AppLayout/updateTemplateHeaderType', {
-        //   headerVisibility: false,
-        //   headerType: 'A',
-        //   sideBarVisibility: false,
-        //   sideBarType: 'A'
-        // })
-        // this.$store.commit('AppLayout/updateTemplateHeaderType', this.$route.name)
         this.templateHeaderType = this.$store.getters['AppLayout/templateHeaderType']
+        this.templateSideBarType = this.$store.getters['AppLayout/templateSideBarType']
       }
     }
   },
   watch: {
-    // 'properties.layoutHeaderCustomClass': function (newVal) {
-    //   console.log('ok')
-    //   this.setLayoutCustomClass()
-    // }
   },
   mounted () {
-    // this.getTemplateHeaderType()
-    // this.templateHeaderType = this.$store.getters['AppLayout/templateHeaderType']
-    // this.templateHeaderVisible = this.$store.getters['AppLayout/templateHeaderType']
-    // this.$store.commit('AppLayout/updateLayoutLeftDrawerVisible', false)
+    this.getTemplateTypes()
   },
   created () {
     this.updateLayout()
@@ -201,7 +189,6 @@ export default {
   background-color: #f1f1f1;
   display: flex;
   flex-direction: row;
-  padding-bottom: 24px;
 
   .header-inside {
     width: 100%;
